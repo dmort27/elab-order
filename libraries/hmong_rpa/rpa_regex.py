f8233231c968999fe8c3deaa4079d2afe8f2dbfe
David R. Mortensen <dmortens@cs.cmu.edu>
2021-09-13 22:21:57 -0400

    Added regexes

---
+++ libraries/hmong_rpa/rpa_regex.py
@@ -0,0 +1,19 @@
+import re
+
+HD_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
+                                n?(?:pl|tx|ts|p|t|r|c|k|q)h?|h?|d|dh|
+                                  h?(?:ny|n|ml|m|l)|)
+                            (?P<rhy>ee|oo|ai|aw|ua|ia|ua|i|e|a|o|u|w)
+                            (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
+
+ML_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
+                                n?(?:dl|pl|tx|ts|p|t|r|c|k|q)h?|
+                                  (?:ny|n|ml|m|hl|l)|)
+                            (?P<rhy>aa|ee|oo|ai|aw|ua|ua|i|e|a|o|u|w)
+                            (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
+
+RPA_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
+                                n?(?:dl|pl|tx|ts|p|t|r|c|k|q)h?|h?|d|dh|
+                                  h?(?:ny|n|ml|m|l)|)
+                            (?P<rhy>aa|ee|oo|ai|aw|ua|ia|ua|i|e|a|o|u|w)
+                            (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
