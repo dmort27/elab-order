5b90b4d62b703f8870e5db5b9324eca1a2fe3421
cuichenx <bob.cui@mail.utoronto.ca>
2021-09-14 16:41:53 -0400

    add data visualization notebook and fix small errors in regex and extracted csv

--- libraries/hmong_rpa/rpa_regex.py
+++ libraries/hmong_rpa/rpa_regex.py
@@ -3,17 +3,17 @@ import re
 HD_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
                                 n?(?:pl|tx|ts|p|t|r|c|k|q)h?|h?|d|dh|
                                   h?(?:ny|n|ml|m|l)|)
-                            (?P<rhy>ee|oo|ai|aw|ua|ia|ua|i|e|a|o|u|w)
+                            (?P<rhy>ee|oo|ai|aw|au|ia|ua|i|e|a|o|u|w)
                             (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
 
 ML_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
                                 n?(?:dl|pl|tx|ts|p|t|r|c|k|q)h?|
                                   (?:ny|n|ml|m|hl|l)|)
-                            (?P<rhy>aa|ee|oo|ai|aw|ua|ua|i|e|a|o|u|w)
+                            (?P<rhy>aa|ee|oo|ai|aw|au|ua|i|e|a|o|u|w)
                             (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
 
 RPA_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
                                 n?(?:dl|pl|tx|ts|p|t|r|c|k|q)h?|h?|d|dh|
                                   h?(?:ny|n|ml|m|l)|)
-                            (?P<rhy>aa|ee|oo|ai|aw|ua|ia|ua|i|e|a|o|u|w)
+                            (?P<rhy>aa|ee|oo|ai|aw|au|ia|ua|i|e|a|o|u|w)
                             (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
