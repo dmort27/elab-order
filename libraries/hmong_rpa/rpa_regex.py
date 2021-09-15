c697128f4d365b99e36142e5298bb7049eb644a3
cuichenx <bob.cui@mail.utoronto.ca>
2021-09-14 21:49:58 -0400

    added preliminary classification with sklearn's decision tree using onehot vectors

--- libraries/hmong_rpa/rpa_regex.py
+++ libraries/hmong_rpa/rpa_regex.py
@@ -4,16 +4,16 @@ HD_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
                                 n?(?:pl|tx|ts|p|t|r|c|k|q)h?|h?|d|dh|
                                   h?(?:ny|n|ml|m|l)|)
                             (?P<rhy>ee|oo|ai|aw|au|ia|ua|i|e|a|o|u|w)
-                            (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
+                            (?P<ton>b|s|j|v|m|g|d|)''', flags=re.X)
 
 ML_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
                                 n?(?:dl|pl|tx|ts|p|t|r|c|k|q)h?|
                                   (?:ny|n|ml|m|hl|l)|)
                             (?P<rhy>aa|ee|oo|ai|aw|au|ua|i|e|a|o|u|w)
-                            (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
+                            (?P<ton>b|s|j|v|m|g|d|)''', flags=re.X)
 
 RPA_SYLLABLE = re.compile('''(?P<ons>f|v|xy|x|s|z|y|h|
                                 n?(?:dl|pl|tx|ts|p|t|r|c|k|q)h?|h?|d|dh|
                                   h?(?:ny|n|ml|m|l)|)
                             (?P<rhy>aa|ee|oo|ai|aw|au|ia|ua|i|e|a|o|u|w)
-                            (?P<ton>b|s|j|v|m|g|)''', flags=re.X)
+                            (?P<ton>b|s|j|v|m|g|d|)''', flags=re.X)
