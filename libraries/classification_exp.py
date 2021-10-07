486d1b3e34622e3c65c3f4d1e2a4d0b89599c542
cuichenx <bob.cui@mail.utoronto.ca>
2021-10-06 23:37:38 -0400

    revamped code structure to be cleaner. added lahu classification code

---
+++ libraries/classification_exp.py
@@ -0,0 +1,73 @@
+from typing import Union
+
+from scipy.stats import describe
+from sklearn.feature_selection import SelectFromModel, SelectKBest, chi2
+from sklearn.metrics import precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.base import clone
+import numpy as np
+
+class ClassificationExperiment:
+    def __init__(self, clf: Union[DecisionTreeClassifier, SVC], X, y):
+        self.clf = clf
+        self.X = X
+        self.y = y
+        self.clf.fit(X, y)
+
+    def get_full_train_acc(self):
+        return self.clf.score(self.X, self.y)
+
+    def train_test(self, X=None, y=None, clf=None, num_runs=50, test_size=0.3):
+        if X is None: X = self.X
+        if y is None: y = self.y
+        if clf is None: clf = self.clf
+        accs = []
+        prns = []
+        rcls = []
+        for rnd in range(num_runs):
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=rnd)
+            clf_train = clf.fit(X_train, y_train)
+            accs.append(clf_train.score(X_test, y_test))
+            y_predict = clf_train.predict(X_test)
+            prns.append(precision_score(y_test, y_predict))
+            rcls.append(recall_score(y_test, y_predict))
+
+        return accs, prns, rcls
+
+    def select_features_from_model(self, method='from_model'):
+        clf_best = None
+        best_accs = 0
+        if self.clf.__class__.__name__ == 'SVC':
+            num_features_to_try = (160, 140, 120, 100, 80, 70, 60)
+        else:
+            num_features_to_try = (120, 110, 100, 90, 80, 70, 60, 50, 40, 35, 30, 25, 20, 15, 10, 8, 6)
+        for t in num_features_to_try:
+            if method == 'from_model':
+                model = SelectFromModel(self.clf, prefit=True, threshold=-np.inf, max_features=t)
+                X_new = model.transform(self.X)
+            elif method == 'chi2':
+                if t > self.X.shape[1]: continue
+                X_new = SelectKBest(chi2, k=t).fit_transform(self.X, self.y)
+            else:
+                raise NotImplementedError
+
+            print(X_new.shape[1], end=', ')
+            clf_new = clone(self.clf).fit(X_new, self.y)
+            print(f"{clf_new.score(X_new, self.y):.3f}", end=', ')
+
+            accs, prns, rcls = self.train_test(X=X_new, clf=clf_new, num_runs=10)
+
+            mean_accs = np.mean(accs)
+            mean_prns = np.mean(prns)
+            mean_rcls = np.mean(rcls)
+            if mean_accs > best_accs:
+                best_accs = mean_accs
+                clf_best = clf_new
+            print(f"{mean_accs:.3f}, prn {mean_prns:.3f}, rcl {mean_rcls:.3f}")
+
+        return best_accs
+
+
+
---
+++ libraries/elab_data.py
@@ -0,0 +1,210 @@
+import math
+from collections import defaultdict
+
+import epitran, panphon
+import pandas as pd
+import numpy as np
+
+class ElaborateExpressionData:
+    def __init__(self, df, features='ton_rhy_ons', lang_id='', syl_regex=None, verbose=True,
+                 remove_dup_ordered=None):
+        self.df = df
+        self.lang_id = lang_id
+        self.syl_regex = syl_regex
+        self.verbose = verbose
+        self.features = features
+        self._reformat_columns()
+        self._remove_invalid_data()
+        self._set_phonemes()
+        if remove_dup_ordered is not None:
+            self._remove_dups(remove_dup_ordered)
+
+    def rule_based_classification(self):
+        def sgn(x):
+            return 0 if x == 0 else x // abs(x)
+
+        if self.lang_id == 'hmn-Latn':
+            orders = {'j': 1,
+                      'b': 2,
+                      'm': 3, 'd': 3,
+                      's': 4,
+                      'v': 5,
+                      'g': 6,
+                      '': 7}
+            rule_pred = (self.df['cc2'].apply(lambda syl: orders[self.syl_regex.match(syl).group("ton")]) -
+                         self.df['cc1'].apply(lambda syl: orders[self.syl_regex.match(syl).group("ton")]))
+            res = rule_pred.apply(sgn).value_counts().to_dict()
+        elif self.lang_id == 'lhu-Latn':
+            orders = {'o': 1,
+                      'u': 2,
+                      'i': 3,
+                      'ɨ': 4,
+                      'ə': 5,
+                      'ɔ': 6,
+                      'e': 7,
+                      'ɛ': 8,
+                      'a': 9}
+            rule_pred = (self.df['cc2'].apply(lambda syl: orders[self.syl_regex.match(syl).group("rhy")]) -
+                         self.df['cc1'].apply(lambda syl: orders[self.syl_regex.match(syl).group("rhy")]))
+            res = rule_pred.apply(sgn).value_counts().to_dict()
+        else:
+            raise NotImplementedError(f"Rule for language {self.lang_id} is not available")
+
+        N = len(self.df)
+        if self.verbose:
+            print(f'Correct: {res[1] / N}')
+            print(f'Tie: {res[0] / N}')
+            print(f'Incorrect: {res[-1] / N}')
+            print(f'Correct with random guess: {res[1] / N + res[0] / N / 2}')
+            print(f'Incorrect with random guess: {res[-1] / N + res[0] / N / 2}')
+            print()
+        return res[1] / N + res[0] / N / 2
+
+    def rule_search(self):
+        def sgn(x):
+            return 0 if x == 0 else x // abs(x)
+        from itertools import permutations
+        N = len(self.df)
+        best = 0
+        if self.lang_id == 'hmn-Latn':
+            permuted = list('jbmdsvg')+['']
+            group = 'ton'
+        elif self.lang_id == 'lhu-Latn':
+            permuted = "iɨueəoɛaɔ"
+            group = 'rhy'
+        else:
+            raise NotImplementedError
+
+        perm = permutations(permuted)
+        for iter in range(math.factorial(len(permuted))):
+            p = next(perm)
+            orders = {p[i]: i for i in range(len(permuted))}
+            rule_pred = (self.df['cc2'].apply(lambda syl: orders[self.syl_regex.match(syl).group(group)]) -
+                         self.df['cc1'].apply(lambda syl: orders[self.syl_regex.match(syl).group(group)]))
+            res = rule_pred.apply(sgn).value_counts().to_dict()
+            correct = res[1] / N + res[0] / N / 2
+            if correct > best:
+                best = correct
+                print(f'iter {iter}, best is {best}, order is {orders}')
+
+    def get_Xy_data(self):
+        print('N =', len(self.df))
+        self._add_unattested_data()
+        self._add_onehot_features(self.features, drop_orig_columns=True)
+        X = self.df.drop(columns=['attested']).to_numpy()
+        y = self.df['attested'].to_numpy()
+        return X, y
+
+    def _reformat_columns(self):
+        '''
+        Converts df from (word1, word2, word3, word4) into (cc1, cc2, rep, is_ABAC)
+        '''
+        if self.df.columns.tolist() == ['cc1', 'cc2', 'rep', 'is_ABAC']: return
+        if self.df.columns.tolist() != ['word1', 'word2', 'word3', 'word4']:
+            print('Cannot reformat df. Need to be in (word1, word2, word3, word4) format')
+            return
+
+        df_ABAC = self.df[self.df.word1 == self.df.word3]
+        df_ABAC = df_ABAC.rename(columns={'word1': 'rep', 'word2': 'cc1', 'word4': 'cc2'}).drop(columns='word3')
+        df_ABAC['is_ABAC'] = True
+        df_ABAC = df_ABAC[['cc1', 'cc2', 'rep', 'is_ABAC']]
+
+        df_ABCB = self.df[self.df.word2 == self.df.word4]
+        df_ABCB = df_ABCB.rename(columns={'word1': 'cc1', 'word2': 'rep', 'word3': 'cc2'}).drop(columns='word4')
+        df_ABCB['is_ABAC'] = False
+        df_ABCB = df_ABCB[['cc1', 'cc2', 'rep', 'is_ABAC']]
+
+        self.df = pd.concat([df_ABAC, df_ABCB], names=['cc1', 'cc2', 'rep', 'is_ABAC'])
+
+    def _remove_invalid_data(self):
+        '''
+        Removes elaborate expressions in df which cannot be parsed by syl_regex
+        '''
+        if self.df.columns.tolist() != ['cc1', 'cc2', 'rep', 'is_ABAC']:
+            print('Cannot remove invalid data. Need to be in (cc1, cc2, rep, is_ABAC) format')
+            return
+
+        def is_valid_syl(syl):
+            m = self.syl_regex.match(syl)
+            if m:
+                ons, rhy, ton = m.group("ons"), m.group("rhy"), m.group("ton")
+                return ons + rhy + ton == syl
+            return False
+
+        c = 0
+        for i, (cc1, cc2, rep, form) in self.df.iterrows():
+            if not all(is_valid_syl(w) for w in (cc1, cc2, rep)):
+                self.df.drop(i, inplace=True)
+                if self.verbose:
+                    print(f"Dropping {i} because one of ({cc1}, {cc2}, {rep}) cannot be parsed")
+            else:
+                c += 1
+        if self.verbose:
+            print(c, 'good elaborate expressions left')
+        self.df.reset_index(drop=True, inplace=True)
+
+    def _set_phonemes(self):
+        all_syllables = set(self.df["cc1"].tolist()).union(set(self.df["cc2"].tolist())).union(set(self.df["rep"].tolist()))
+        self.onsets, self.rhymes, self.tones = set([]), set([]), set([])
+        for syl in all_syllables:
+            m = self.syl_regex.match(syl)
+            ons, rhy, ton = m.group("ons"), m.group("rhy"), m.group("ton")
+            self.onsets.add(ons)
+            self.rhymes.add(rhy)
+            self.tones.add(ton)
+
+    def _remove_dups(self, ordered):
+        inverted_idx = defaultdict(list)
+        if ordered:
+            for i, (cc1, cc2, _, _) in self.df.iterrows():
+                inverted_idx[(cc1, cc2)].append(i)
+        else:
+            for i, (cc1, cc2, _, _) in self.df.iterrows():
+                inverted_idx[(max(cc1, cc2), min(cc1, cc2))].append(i)
+        use_indices = []
+        for values in inverted_idx.values():
+            use_indices.append(np.random.choice(values))
+        self.df = self.df.iloc[use_indices].reset_index(drop=True)
+
+    def _add_unattested_data(self):
+        '''
+        for each (cc1, cc2) phrase, add (cc2, cc1) phrase
+        '''
+        unique_order_indices = []
+        for i, (cc1, cc2, rep, form) in self.df.iterrows():
+            other_order = self.df[(self.df.rep == rep) & (self.df.cc1 == cc2) & (self.df.cc2 == cc1)]
+            if len(other_order) == 0:
+                unique_order_indices.append(i)
+        self.df['attested'] = True
+        unattested = self.df.rename(columns={'cc1': 'cc2', 'cc2': 'cc1'}).iloc[unique_order_indices]
+        unattested['attested'] = False
+        if self.verbose:
+            print(f'len of attested {len(self.df)}, len of unattested {len(unattested)}')
+        self.df = self.df.append(unattested, ignore_index=True)
+
+    def _add_onehot_features(self, features, drop_orig_columns=True):
+        if 'panphon' in features:
+            epi = epitran.Epitran(self.lang_id)
+            ft = panphon.FeatureTable()
+
+        for wi in ('cc1', 'cc2', 'rep'):
+            if 'ton' in features:
+                for ton in self.tones:
+                    self.df[f'{wi}_ton_a{ton}'] = self.df[wi].apply(lambda syl: self.syl_regex.match(syl).group("ton") == ton)
+            if 'rhy' in features:
+                for rhy in self.rhymes:
+                    self.df[f'{wi}_rhy_{rhy}'] = self.df[wi].apply(lambda syl: self.syl_regex.match(syl).group("rhy") == rhy)
+            if 'ons' in features:
+                for ons in self.onsets:
+                    self.df[f'{wi}_ons_{ons}'] = self.df[wi].apply(lambda syl: self.syl_regex.match(syl).group("ons") == ons)
+            if 'panphon' in features:
+                wordi_feats = self.df[wi].apply(lambda syl: ft.bag_of_features(epi.transliterate(syl)))
+                ## TODO: panphone feature for ons and rhy separately?
+                panphon_names = [f'{wi}_{sign}{n}' for n in ft.names for sign in ('+', '0', '-')]
+                self.df = pd.merge(
+                        self.df,
+                        pd.DataFrame(wordi_feats.tolist(), index=self.df.index, columns=panphon_names),
+                        left_index=True, right_index=True)
+
+        if drop_orig_columns:
+            self.df.drop(columns=['cc1', 'cc2', 'rep'], inplace=True)
